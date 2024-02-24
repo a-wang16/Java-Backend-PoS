7e842d535a62b10a49bddcc4b569f833fabcde0a
karlos <karloszuru@gmail.com>
2024-02-24 12:30:06 -0600

    Some boiler plate files.

--- frontend/src/main/java/com/example/frontend/HelloController.java
+++ frontend/src/main/java/com/example/frontend/HelloController.java
@@ -1,7 +1,14 @@
 package com.example.frontend;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
@@ -10,5 +17,20 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    private Button switchViewButton;
+
+    @FXML
+    private void switchToManagerView() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("manager-view.fxml"));
+        Parent root = loader.load();
+
+        Stage stage = (Stage) switchViewButton.getScene().getWindow();
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
