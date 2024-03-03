f84d970b302b4bbe10dc0337d759e2f8d6747d4d
“Gemma <122994950+gemmagoddard3@users.noreply.github.com.>
2024-03-02 19:09:10 -0600

    Adding Product Usage Chart

--- frontend/src/main/java/com/example/frontend/ManagerGraphViewController.java
+++ frontend/src/main/java/com/example/frontend/ManagerGraphViewController.java
@@ -4,11 +4,16 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.sql.*;
-import java.util.ArrayList;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.DatePicker;
+
+import java.time.LocalDate;
 import java.util.Properties;
 import java.util.ResourceBundle;
-//
 import javafx.animation.TranslateTransition;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -18,20 +23,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.PieChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.TilePane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontPosture;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -50,8 +49,14 @@ public class ManagerGraphViewController implements Initializable{
     private AnchorPane slide_menu;
     @FXML
     private ImageView switchBtn;
+    @FXML
+    private Button productUsageButton;
     private Stage primaryStage;
     private Boolean employeeView;
+    @FXML
+    private DatePicker productEndDate;
+    @FXML
+    private DatePicker productStartDate;
     Connection conn;
 
     private Properties readProperties() {
@@ -129,11 +134,13 @@ public class ManagerGraphViewController implements Initializable{
     }
 
     public void showSalesTrend(ActionEvent event){
+        graphContainer.getChildren().clear();
+        ScrollPane graphScroll = new ScrollPane();
+        graphScroll.setPrefWidth(750);
         try{
             String sqlStatement = "SELECT EXTRACT(WEEK FROM co.Created_At) AS OrderWeek, SUM(oi.Quantity * mi.Price) AS TotalRevenue FROM Order_Items oi JOIN Menu_Item mi ON oi.Menu_Item_ID = mi.ID JOIN Customer_Order co ON oi.Order_ID = co.ID GROUP BY OrderWeek ORDER BY OrderWeek;";
             Statement stmt = conn.createStatement();
             ResultSet result = stmt.executeQuery(sqlStatement);
-
 
             XYChart.Series series = new XYChart.Series();
             int week = 0;
@@ -156,8 +163,8 @@ public class ManagerGraphViewController implements Initializable{
             LineChart linechart = new LineChart(xAxis, yAxis);
             linechart.getData().add(series);
             linechart.setPrefWidth(900);
-            graphContainer.getChildren().add(linechart);
-
+            graphScroll.setContent(linechart);
+            graphContainer.getChildren().add(graphScroll);
 
         } catch (Exception e){
             e.printStackTrace();
@@ -168,6 +175,42 @@ public class ManagerGraphViewController implements Initializable{
 
     }
 
+    public void showProductUsage(ActionEvent event){
+        graphContainer.getChildren().clear();
+        ScrollPane graphScroll = new ScrollPane();
+        graphContainer.getChildren().add(graphScroll);
+        String name = "";
+        int qty = 0;
+        try{
+            LocalDate startDate = productStartDate.getValue();
+            String start = startDate.getMonthValue() + "/" + startDate.getDayOfMonth() + "/" + startDate.getYear();
+            LocalDate endDate = productEndDate.getValue();
+            String end = endDate.getMonthValue() + "/" + endDate.getDayOfMonth() + "/" + endDate.getYear();
+
+            String sqlStatement = "SELECT i.Name, SUM(r.qty) AS inventory_used FROM Customer_Order co, Menu_Item mi JOIN Recipe r ON mi.ID = r.Menu_item JOIN Inventory i ON r.Inventory_item = i.ID WHERE Created_At >= '" + start +"' AND Created_At < '" + end +"' GROUP BY i.Name ORDER BY inventory_used DESC;";
+            Statement stmt = conn.createStatement();
+            ResultSet result = stmt.executeQuery(sqlStatement);
+
+            ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
+
+            while(result.next()) {
+                name = result.getString("name");
+                qty = result.getInt("inventory_used");
+                pieChartData.add(new PieChart.Data(name, qty));
+            }
+            PieChart chart = new PieChart(pieChartData);
+            chart.setTitle("Product Usage From: " + start + " - " + end);
+            chart.setPrefSize(600, 400);
+            graphScroll.setPadding(new Insets(20, 75, 20,75));
+            graphScroll.setContent(chart);
+
+        } catch (Exception e){
+            e.printStackTrace();
+            System.out.println("Error accessing Database.");
+        }
+    }
+
+
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
         menu_close.setVisible(false);
@@ -175,8 +218,10 @@ public class ManagerGraphViewController implements Initializable{
         slide_menu.setTranslateX(-100);
 
         salesTrendButton.setOnAction(this::showSalesTrend);
+        productUsageButton.setOnAction((this::showProductUsage));
 
         conn = DatabaseConnectionManager.getConnection();
+
 
 //       try{
 //
