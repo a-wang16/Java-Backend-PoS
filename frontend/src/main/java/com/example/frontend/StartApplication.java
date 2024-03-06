a0e743b216dd111f1d8614cb07f710df1327e9dc
jinss5 <jinss1145@gmail.com>
2024-03-06 16:14:33 -0600

    start application javadoc updated

--- frontend/src/main/java/com/example/frontend/StartApplication.java
+++ frontend/src/main/java/com/example/frontend/StartApplication.java
@@ -7,7 +7,20 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * This class extends the JavaFX Application class.
+ * It serves as the main entry point for the JavaFX application.
+ * @author Jin Seok Oh
+ */
 public class StartApplication extends Application {
+
+    /**
+     * Starts the primary stage for this application.
+     *
+     * @param stage The primary stage for this application, onto which
+     *              the application scene can be set. The primaryStage is created by the JavaFX platform.
+     * @throws IOException if loading the fxml file fails
+     */
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("login.fxml"));
@@ -17,6 +30,14 @@ public class StartApplication extends Application {
         stage.setTitle("Login");
         stage.show();
     }
+    
+    /**
+     * The main method to launch the application.
+     * This method is the entry point when the application is launched from the command line.
+     *
+     * @param args Command line arguments passed to the launch method.
+     */
+
     public static void main(String[] args) {
         launch();
     }
